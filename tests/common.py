2e9cd34e4970c3d9510d4fc3bcb018498bd40408
brianmichalski <90117970+brianmichalski@users.noreply.github.com>
2024-12-01 00:20:08 -0500

    load + stress tests (refactoring)

--- tests/common.py
+++ tests/common.py
@@ -1,26 +1,40 @@
+from datetime import datetime
+import os
 import numpy as np
+from requests import Session
 
-def user_journey(session, base_url):
+
+def user_journey(session: Session, base_url, check_response=False):
     # Step 1: Open main page
     main_url = f"{base_url}/"
-    session.get(main_url)
+    response = session.get(main_url)
+    if check_response and not response.ok:
+        return False
 
     # Step 2: Go to login page
     login_url = f"{base_url}/login"
     session.get(login_url)
+    if check_response and not response.ok:
+        return False
 
     # Step 3: Perform the login
     login_url = f"{base_url}/check-login"
     payload = {"email": "onlinenewssite@admin.com", "password": "123456789"}
     session.post(login_url, data=payload)
+    if check_response and not response.ok:
+        return False
 
     # Step 4: Go to posts page
     posts_url = f"{base_url}/admin/post"
     session.get(posts_url)
+    if check_response and not response.ok:
+        return False
 
     # Step 5: Go to post create page
     create_post_url = f"{base_url}/admin/post/create"
     session.get(create_post_url)
+    if check_response and not response.ok:
+        return False
 
     # Step 6: Save the post (form-data with file upload)
     post_url = f"{base_url}/admin/post/store"
@@ -40,31 +54,34 @@ def user_journey(session, base_url):
             "body": "Body test...",
         }
         session.post(post_url, data=data, files=files)
+    if check_response and not response.ok:
+        return False
 
     # Step 7: Search for the post
     search_url = f"{base_url}/?Search-box=Summary test..."
     session.get(search_url)
+    if check_response:
+        return response.ok
 
-def generate_user_load(start=10, max_users=1000, steps=100, growth_rate=0.1):
-    """
-    Generates a list of user load values that increase over time with a logarithmic growth pattern.
 
-    :param start: Starting number of users.
-    :param max_users: Maximum number of users (the upper limit).
-    :param steps: Number of steps or time intervals (how many data points).
-    :param growth_rate: Rate at which the users increase.
-    :return: List of user loads over time.
-    """
-    # generate time intervals
-    time_intervals = np.arange(steps)
-    
-    # calculate user load using a logarithmic growth formula
-    user_load = start + (max_users - start) * np.log1p(growth_rate * time_intervals)
-    
-    # ensure the user load does not exceed max_users
-    user_load = np.clip(user_load, start, max_users)
-    
-    # round the values 
-    user_load = np.round(user_load).astype(int)
-    
-    return user_load
+def generate_user_load(start=1, max_users=1000, steps=100):
+    return np.linspace(start, max_users, steps).astype(int)
+
+
+def prepare_file(path: str):
+    # Delete the output file if it exists
+    if os.path.exists(path):
+        os.remove(path)
+    else:
+        # Create directories if needed
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+
+def get_base_url():
+    return "http://192.168.33.20/OnlineNewsSite"
+
+
+def get_results_filename(script_name):
+    curr_time = datetime.now().strftime("%Y-%m-%d_%H-%M")
+    test_name = script_name.split(".")[0]
+    return f"results/{test_name}/{test_name}_{curr_time}.csv"
--- tests/load_test.py
+++ tests/load_test.py
@@ -1,18 +1,40 @@
-import requests
-import time
-import threading
 import concurrent.futures
 import csv
 import os
+import sys
+import requests
+import statistics
+import time
 from tests import common
+import threading
+
+# Access the command-line arguments
+if len(sys.argv) < 2:
+    print("Provide the argument for the type of test  ('load' or 'stress')")
+    sys.exit()
+
+test_type = sys.argv[1]
 
 ##################### SETUP #####################
 # Base URL
-base_url = "http://192.168.33.20/OnlineNewsSite"
+base_url = common.get_base_url()
 # Results file
-output_file = "results/load_test_results.csv"
+output_file = common.get_results_filename(f"{test_type}_test")
 # Thread-local storage for session objects
 thread_local = threading.local()
+# Simultaneous threads
+max_workers = 5
+# Load test: number of simulated users (threads)
+users_list_load = [ 1, 2, 3, 4, 5, 6, 10, 16, 21, 27, 32, 37, 42, 47, 53, 58, 63, 68, 73, 79,
+              84, 89, 94, 100, 119, 144, 170, 196, 221, 247, 273, 298, 324, 350, 369, 439, 
+              509, 579, 649, 719, 789, 859, 929, 1000]
+# Stress test: number of simulated users
+users_list_stress = [1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000]
+# Defines the set of simulated users depending on the type of test
+# if it is stress test, join both arrays, otherwise, uses only load set
+users_list = users_list_load
+if test_type == 'stress':
+    users_list += users_list_stress
 #################################################
 
 # Dictionary to hold execution times (response time per user)
@@ -26,9 +48,11 @@ def get_session():
     return thread_local.session
 
 
-def perform_load_test(num_users):
+def perform_test(num_users):
     """Perform the entire load test using the specified number of users."""
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    with concurrent.futures.ThreadPoolExecutor(
+        max_workers=max_workers, thread_name_prefix=f"{num_users}_"
+    ) as executor:
         # Perform all user journeys and then collect response times
         response_times = list(executor.map(test_user_flow, range(num_users)))
 
@@ -38,53 +62,51 @@ def perform_load_test(num_users):
 
 def test_user_flow(user_id):
     """Simulate a user performing the full flow."""
+    # Starting time (for each thread)
+    start_time = time.time()
     try:
-        # Starting time (for each thread)
-        start_time = time.time()
-
         # Perform the requests to the website
         common.user_journey(get_session(), base_url)
-
-        # Duration of the thread execution
-        duration = time.time() - start_time
-
     except requests.RequestException as e:
         # Log any request exception (e.g., connection error, timeout, etc.)
         print(f"Error encountered: {user_id}: {e}")
-        duration = None  # If there's an error, record it as None
+
+    finally:
+        # Duration of the thread execution
+        duration = time.time() - start_time
 
     return duration
 
 
 if __name__ == "__main__":
     # Prepare the CSV file with headers (if not already exists)
-    try:
-        # Delete the output file if it exists
-        if os.path.exists(output_file):
-            os.remove(output_file)
-        # Create the output file and print the header
-        with open(output_file, "x", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow(["Number of users", "Response Time (seconds)"])
-    except FileExistsError:
-        # File already exists, no need to write the header again
-        pass
+    common.prepare_file(output_file)
+    # Create the output file and print the header
+    with open(output_file, "x", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            [
+                "Number of users",
+                "AVG Response Time",
+                "Median Response Time",
+                "Actual Number of Users",
+            ]
+        )
 
-    # Number of simulated users
-    numbers = common.generate_user_load(
-        start=1, max_users=200, steps=30, growth_rate=0.015
-    )
-
-    for num_users in numbers:
-        perform_load_test(num_users)
+    for num_users in users_list:
+        perform_test(num_users)
 
         # Calculate the average response time for the current number of users
         response_times = execution_time[num_users]
         avg_response_time = sum(response_times) / len(response_times)
+        median_response_time = statistics.median(response_times)
+        actual_num_users = len(response_times)
 
         # Write the results to the CSV file
         with open(output_file, "a", newline="") as file:
             writer = csv.writer(file)
-            writer.writerow([num_users, avg_response_time])
+            writer.writerow(
+                [num_users, avg_response_time, median_response_time, actual_num_users]
+            )
 
     print(f"Load test completed. Results saved to {output_file}")
